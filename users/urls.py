9ffaf3a76f4a29c42a9b46de523a4c676efdecb3
brocode <kelimbth@gmail.com>
2023-04-07 13:53:42 +0300

    new URL paths

---
+++ users/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index_view, name='index'),
+    path('homepage/', views.homepage_view, name='homepage'),
+    
+]
