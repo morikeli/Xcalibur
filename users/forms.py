9ab37d2245da8c04247c5f6f4855debeafb78558
brocode <kelimbth@gmail.com>
2023-04-07 13:52:35 +0300

    updated form to select lang. accent

--- users/forms.py
+++ users/forms.py
@@ -3,6 +3,15 @@ from django import forms
 
 
 class GenerateAudioFileForm(forms.ModelForm):
+    SELECT_ACCENT = (
+        ('co.uk', '-- Select one option --'),
+        ('com.au', 'English (Australia)'),
+        ('ca', 'English (Canada)'),
+        ('co.in', 'English (India)'),
+        ('ie', 'English (Ireland)'),
+        ('com.uk', 'English (UK)'),
+        ('us', 'English (US)'),
+    )
     SELECT_FILE_TYPE = (
         (None, '-- Select audio format --'),
         ('.mp3', '.mp3'),
@@ -19,6 +28,11 @@ class GenerateAudioFileForm(forms.ModelForm):
         widget=forms.Textarea(attrs={'type': 'text', 'class': 'mt-2', 'placeholder': 'Type your text ...'}),
         label='Text',
         help_text='This text that will be use to generate an audio file',
+        )
+    accent = forms.ChoiceField(
+        widget=forms.Select(attrs={'type': 'select', 'class': 'mt-2'}),
+        help_text='Select your preferred accent',
+        choices=SELECT_ACCENT,
         )
     file_type = forms.ChoiceField(widget=forms.Select(attrs={'type': 'select', 'class': 'mt-2 mb-2'}), choices=SELECT_FILE_TYPE)
     
