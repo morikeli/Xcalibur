1c6e8d0837fcbb21d06b3fe44730fb5fe5e6d6e7
brocode <kelimbth@gmail.com>
2023-04-07 12:42:07 +0300

    updated admin panel

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.admin import UserAdmin
 from django.contrib import admin
+from .models import User
+from .forms import SignupForm
+from .models import User
 
-# Register your models here.
+class UserLayout(UserAdmin):
+    model = User
+    add_form = SignupForm
+    list_display = ['username', 'email', 'gender', 'is_staff', 'date_joined',]
+
+admin.site.register(User, UserLayout)
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import ChatBot, Audios
 
-# Register your models here.
+
+@admin.register(ChatBot)
+class ChatBotInfoTable(admin.ModelAdmin):
+    list_display = ['name', 'speaker', 'created']
+    readonly_fields = ['speaker']
+
+
+@admin.register(Audios)
+class AudioFilesRecordsTable(admin.ModelAdmin):
+    list_display = ['name', 'title', 'file_type', 'created']
+    readonly_fields = ['title', 'description', 'file_type', 'created']
