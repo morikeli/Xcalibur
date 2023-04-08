286dad47c8941ff27e72ee17ae830a06485c87e6
brocode <kelimbth@gmail.com>
2023-04-08 12:23:58 +0300

    updated table attrs.

--- users/models.py
+++ users/models.py
@@ -20,11 +20,11 @@ class ChatBot(models.Model):
 class Audios(models.Model):
     id = models.CharField(max_length=25, primary_key=True, unique=True, editable=False)
     name = models.ForeignKey(User, on_delete=models.CASCADE, editable=False)
-    title = models.CharField(max_length=50, blank=False)
+    title = models.CharField(max_length=50, blank=True)
     description = models.TextField(blank=False)
     audio = models.FileField(upload_to='Audios/', null=False, editable=False)
     accent = models.CharField(max_length=20, blank=True)
-    file_type = models.CharField(max_length=4, blank=False)
+    file_type = models.CharField(max_length=4, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     edited = models.DateTimeField(auto_now=True)
 
