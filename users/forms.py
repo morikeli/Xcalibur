d57a01a452b1afbdb2db17fb8e5a55ddf581d85e
brocode <kelimbth@gmail.com>
2023-04-09 13:39:05 +0300

    created form to allow users to upload audio files

--- users/forms.py
+++ users/forms.py
@@ -1,3 +1,4 @@
+from django.core.validators import FileExtensionValidator
 from .models import Audios
 from django import forms
 
@@ -45,7 +46,39 @@ class GenerateAudioFileForm(forms.ModelForm):
 
 
 class UploadAudioFileForm(forms.ModelForm):
-
+    SELECT_ACCENT = (
+        ('co.uk', '-- Select one option --'),
+        ('com.au', 'English (Australia)'),
+        ('ca', 'English (Canada)'),
+        ('co.in', 'English (India)'),
+        ('ie', 'English (Ireland)'),
+        ('com.uk', 'English (UK)'),
+        ('us', 'English (US)'),
+    )
+    SELECT_FILE_TYPE = (
+        (None, '-- Select audio format --'),
+        ('.mp3', '.mp3'),
+        ('.ogg', '.ogg'),
+        ('.wav', '.wav')
+    )
+    
+    title = forms.CharField(
+        widget=forms.TextInput(attrs={'type': 'text', 'placeholder': 'What will be the name of your audio file?'}),
+        label='Name',
+        required=True,
+        )
+    accent = forms.ChoiceField(
+        widget=forms.Select(attrs={'type': 'select'}),
+        help_text='Select your preferred accent',
+        label='Language accent',
+        choices=SELECT_ACCENT,
+        )
+    audio = forms.FileField(
+        widget=forms.FileInput(attrs={'type': 'file', 'class': 'form-control'}),
+        help_text='Upload audio files: .mp3, .ogg or .wav only!',
+        validators=[FileExtensionValidator(['mp3', 'ogg', 'wav'])]
+    )
+    
     class Meta:
         model = Audios
-        fields = []
+        fields = ['audio', 'title', 'accent']
