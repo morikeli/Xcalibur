2871824085ae97e402d6999f583b2018ae4df002
brocode <kelimbth@gmail.com>
2023-04-09 13:40:26 +0300

    updated homepage_view() + made minor changes

--- users/views.py
+++ users/views.py
@@ -3,11 +3,12 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib import messages
 from django.conf import settings
-from .forms import GenerateAudioFileForm
+from .forms import GenerateAudioFileForm, UploadAudioFileForm
 from authentication.models import User
 import speech_recognition as sr
 from gtts import gTTS
 import uuid
+import os
 
 
 def index_view(request):
@@ -39,18 +40,18 @@ def index_view(request):
             # Check if the user is authenticated
             if request.user.is_authenticated is False:
                 anonymous_user_username = "AnonymousUser" + str(uuid.uuid4()).replace('-', '')[:10]
-                anonymous_user_email = "AnonUser" + str(uuid.uuid4()).replace('-', '')[:7] + "@unknown-email.x"
+                anonymous_user_email = "anonymoususer" + str(uuid.uuid4()).replace('-', '')[:4] + "@unknown-email.x"
                 user = User.objects.create_user(username=anonymous_user_username, email=anonymous_user_email)
                 user.save()
 
                 new_audio_file.name = user
             
-                gen_audio = str('Anonymous/') + str(new_audio_file.title)+str(new_audio_file.file_type)
+                gen_audio = str('Anonymous/') + str(new_audio_file.title) + str(new_audio_file.file_type)
                 tts.save(str(settings.MEDIA_ROOT) + '/' + gen_audio)
                 new_audio_file.save()
 
                 # automatically download the audio file.
-                # a prompt may appear on some browsers.
+                # a prompt may appear on some browsers -> depending on user's browser configuration.
                 audio_file = str(settings.MEDIA_ROOT) + '/' + gen_audio
                 response = HttpResponse(open(audio_file, 'rb').read())
                 response['Content-Type'] = 'audio/' + gen_audio[-3:]     # set Content-type to "audio/wav" or "audio/mp3" or "audio/ogg"
@@ -59,7 +60,7 @@ def index_view(request):
 
             else:
                 new_audio_file.name = request.user
-                gen_audio = str('Anonymous/') + str(new_audio_file.title)+str(new_audio_file.file_type)
+                gen_audio = str('Anonymous/') + str(new_audio_file.title) + str(new_audio_file.file_type)
                 tts.save(str(settings.MEDIA_ROOT) + '/' + gen_audio)
 
                 new_audio_file.save()
@@ -73,14 +74,27 @@ def index_view(request):
 
 
 @login_required(login_url='login')
-@user_passes_test(lambda user: user.is_staff is False and user.is_superuser is True)
+@user_passes_test(lambda user: user.is_staff is False and user.is_superuser is False)
 def homepage_view(request):
-    
+    audio_form  = UploadAudioFileForm()    
 
     if request.method == 'POST':
-        pass
+        audio_form = UploadAudioFileForm(request.POST, request.FILES)
+        if audio_form.is_valid():
+            form = audio_form.save(commit=False)
+
+            r = sr.Recognizer()
+            file = sr.AudioFile(form.audio)
+            with file as source:
+                audio = r.record(source)
+                r.recognize_google(audio)
+
+                print(f'Audio: {r.recognize_google(audio)}')
+
+            messages.success(request, 'Form submitted successfuly!')
+            return redirect('homepage')
 
 
-    context = {'GenerateAudioFileForm',}
-    return render(request, 'users/', context)
+    context = {'UploadAudioForm': audio_form}
+    return render(request, 'users/homepage.html', context)
 
