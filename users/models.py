4f7df2bc3805337a727ed3e283f8519dee7b1beb
brocode <kelimbth@gmail.com>
2023-04-10 19:17:46 +0300

    created new table

--- users/models.py
+++ users/models.py
@@ -17,13 +17,12 @@ class ChatBot(models.Model):
         verbose_name_plural = 'Chat Bot Records'
         
 
-class UserFiles(models.Model):
+class AudioFiles(models.Model):
     id = models.CharField(max_length=25, primary_key=True, unique=True, editable=False)
     name = models.ForeignKey(User, on_delete=models.CASCADE, editable=False)
     title = models.CharField(max_length=50, blank=True)
     description = models.TextField(blank=False)
     audio = models.FileField(upload_to='Audios/', null=True, default='None')
-    video = models.FileField(upload_to='Videos/', null=True, default='None')
     accent = models.CharField(max_length=20, blank=True)
     file_type = models.CharField(max_length=4, blank=True)
     created = models.DateTimeField(auto_now_add=True)
@@ -34,5 +33,21 @@ class UserFiles(models.Model):
     
     class Meta:
         ordering = ['name', 'created']
-        verbose_name_plural = 'User Files Records'
+        verbose_name_plural = 'Audio Files Records'
 
+
+class VideoFiles(models.Model):
+    id = models.CharField(max_length=25, primary_key=True, unique=True, editable=False)
+    name = models.ForeignKey(User, on_delete=models.CASCADE, editable=False)
+    title = models.CharField(max_length=50, blank=True)
+    video = models.FileField(upload_to='Videos/', null=True, default='None')
+    file_type = models.CharField(max_length=4, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    edited = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.name}'
+    
+    class Meta:
+        ordering = ['name', 'created']
+        verbose_name_plural = 'Video Files Records'
