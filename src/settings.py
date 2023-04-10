067ff71c949db333126c9857c73aecea7fa24a91
brocode <kelimbth@gmail.com>
2023-04-11 02:22:51 +0300

    minor changes

--- src/settings.py
+++ src/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh', 'localhost']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh', 'brocode.pythonanywhere.com', 'localhost']
 
 
 # Application definition
