87bdb5854542834c45e81bf0ad79cc3ca85e412e
brocode <kelimbth@gmail.com>
2023-04-11 01:18:07 +0300

    testing

--- src/settings.py
+++ src/settings.py
@@ -128,7 +128,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR/'media/')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-AUTH_USER_MODEL = 'authentication.user'
+AUTH_USER_MODEL = 'accounts.user'
 
 LOGIN_REDIRECT_URL = 'homepage'
 
