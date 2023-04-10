31ed790d2b3044dcb86723a2bfb095930212c790
brocode <kelimbth@gmail.com>
2023-04-10 21:28:01 +0300

    added domain names

--- src/settings.py
+++ src/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh', 'localhost']
 
 
 # Application definition
