d91952fb60391c9e62729d6f0aa129f55f8bfba9
brocode <kelimbth@gmail.com>
2023-04-11 00:50:36 +0300

    minor change

--- build_files.sh
+++ build_files.sh
@@ -9,6 +9,6 @@ python3.9 manage.py makemigrations
 python3.9 manage.py migrate
 
 echo "Collecting static files ..."
-python3 manage.py collectstatic --noinput --clear
+python3.9 manage.py collectstatic --noinput --clear
 
 echo "BUILD END"
