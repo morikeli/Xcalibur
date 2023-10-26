4555b04bdb067e2583ddc040b17549aa74adcfb5
brocode <kelimbth@gmail.com>
2023-10-26 13:04:12 +0300

    updated urls patterns & imports

--- src/urls.py
+++ src/urls.py
@@ -1,8 +1,8 @@
-from django.contrib import admin
-from django.urls import path, include
-
-from django.conf import settings
+from django.urls import path, include, re_path
 from django.conf.urls.static import static
+from django.views.static import serve
+from django.contrib import admin
+from django.conf import settings
 
 admin.site.site_title = 'Administrator'
 admin.site.site_header = 'XCalibur'
@@ -12,4 +12,5 @@ urlpatterns = [
     path('accounts/', include('authentication.urls')),
     path('', include('users.urls')),
     path('admin/', admin.site.urls),
+    re_path(r'^files/(?P<path>.*)$',serve,{'document_root':settings.MEDIA_ROOT}),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
